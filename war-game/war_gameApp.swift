5085a419e70589f4e5e95de504c858d41df36a1d
Samet Cagri Aktepe <sametcagriaktepe@gmail.com>
2023-02-25 16:24:40 +0100

    Initial Commit

---
+++ war-game/war_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_gameApp.swift
+//  war-game
+//
+//  Created by Samet Cagri Aktepe on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct war_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
